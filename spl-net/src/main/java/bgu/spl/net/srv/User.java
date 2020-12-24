3dd5316a06ab1e10ea6082c914f64bf1f2b41897
Lior Fradin <khtur9512300@walla.com>
2020-12-24 15:33:45 +0200

    user 1.2

--- spl-net/src/main/java/bgu/spl/net/srv/User.java
+++ spl-net/src/main/java/bgu/spl/net/srv/User.java
@@ -9,15 +9,18 @@ import java.util.List;
 public class User {
     private String user;
     private String password;
-    static int[] courseArray;
-    List<Integer> courseIndex;
+    private static int[] courseArray;
+    private List<Integer> courseIndex;
+    private boolean isAdmin;
 
-    public User(String _user, String _password){
+    public User(String _user, String _password, boolean _isAdmin){
         user = _user;
         password =_password;
+        isAdmin = _isAdmin;
         courseIndex = new ArrayList<>();
     }
 
+    // courses ordered by Course.txt
     public static void initializeCourseArray(String coursesFilePath){
         File file = new File(coursesFilePath);
         List<Integer> listCourse = new LinkedList<>();
@@ -42,43 +45,55 @@ public class User {
     catch (IOException ex) { }
     }
 
-    //throws exception if no such course
-    public void registerCourse(Course course) throws Error{
-        boolean registered = false;
-        if(course.getNumOfRegisteredStudents() == course.getNumOfMaxStudents())
-            throw new Error ("course is full");
+    //register students to course
+    public void registerToCourse(Course course) throws Error{
+        synchronized(course) {
+            boolean registered = false;
+            if (course.getNumOfRegisteredStudents() == course.getNumOfMaxStudents())
+                throw new Error("course is full");
 
-        int courseNum = course.getCourseNum();
-        for(int indexOfCourse = 0; indexOfCourse<courseArray.length && !registered ; indexOfCourse++){
-            if(courseArray[indexOfCourse] == courseNum){
-                //insert into list by index size
-                int insertIndex = 0 ;
-                Iterator<Integer> it = courseIndex.iterator();
-                if(!it.hasNext()) {
-                    courseIndex.add(insertIndex, indexOfCourse);
-                    registered = true;
-                }
-                else{
-                    int listIndexPrevValue = -1;
-                    while (it.hasNext()){
-                        int listIndexValue = it.next();
-                        if(indexOfCourse == listIndexValue)
-                            throw new Error("already registered");
-                        else if(listIndexPrevValue < indexOfCourse && indexOfCourse < listIndexValue) {
-                            courseIndex.add(insertIndex, indexOfCourse);
+            int courseNum = course.getCourseNum();
+            for (int indexOfCourse = 0; indexOfCourse < courseArray.length && !registered; indexOfCourse++) {
+                if (courseArray[indexOfCourse] == courseNum) {
+                    //insert into list by index size
+                    int insertIndex = 0;
+                    Iterator<Integer> it = courseIndex.iterator();
+                    if (!it.hasNext()) {
+                        courseIndex.add(insertIndex, indexOfCourse);
+                        registered = true;
+                    } else {
+                        int listIndexPrevValue = -1;
+                        while (it.hasNext() & !registered) {
+                            int listIndexValue = it.next();
+                            if (indexOfCourse == listIndexValue)
+                                throw new Error("already registered");
+                            else if (listIndexPrevValue < indexOfCourse && indexOfCourse < listIndexValue) {
+                                courseIndex.add(insertIndex, indexOfCourse);
+                                registered = true;
+                            }
+                            listIndexPrevValue = listIndexValue;
+                            insertIndex++;
                         }
-                        listIndexPrevValue = listIndexValue;
-                        insertIndex++;
                     }
                 }
             }
+            if (registered)
+                course.registerStudent(this);
+            else
+                throw new Error(" no such course");
         }
-        if(!registered)
-            throw new Error(" no such course");
     }
 
-    public Integer[] getCourseArray(){
+    public Integer[] getNumCourseArray(){
         return (Integer[])courseIndex.toArray();
+    }
+
+    public boolean isAdmin(){
+        return isAdmin;
+    }
+
+    public List<Integer> getListOfCourseIndex(){
+        return courseIndex;
     }
 
 }
