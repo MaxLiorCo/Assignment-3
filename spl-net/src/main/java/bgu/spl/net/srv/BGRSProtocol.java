fa4468100c0ce4b4407bd90aac8ccd601c139772
Lior Fradin <khtur9512300@walla.com>
2020-12-24 17:17:20 +0200

    pipipipi

--- spl-net/src/main/java/bgu/spl/net/srv/BGRSProtocol.java
+++ spl-net/src/main/java/bgu/spl/net/srv/BGRSProtocol.java
@@ -3,15 +3,53 @@ package bgu.spl.net.srv;
 import bgu.spl.net.api.MessagingProtocol;
 
 public class BGRSProtocol implements MessagingProtocol<String> {
+    private Short opcode;
+    private int currParam;
+    private String response;
+    private Database db;
 
-
+    public BGRSProtocol(){
+        Short opcode = null;
+        int currParam = 0;
+        String response = null;
+        db = Database.getInstance();
+    }
     @Override
     public String process(String msg) {
-        return null;
+
+        //beginning new request from client
+        if( opcode == null){
+            opcode = Short.parseShort(msg);
+            response = null;
+        }
+
+        switch(opcode) {
+            case 1:
+                response = ADMINREG();
+            case 2:
+                response = STUDENTREG();
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+        }
+        return response;
     }
 
+    private String ADMINREG(){
+        return response;
+    }
+
+    private String STUDENTREG(){
+        return response;
+    }
     @Override
     public boolean shouldTerminate() {
-        return false;
+        return false; // TODO: change
     }
 }
