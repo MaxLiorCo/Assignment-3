1490727794b9f93bb6144f90ececf5c4b954fe5c
Lior Fradin <khtur9512300@walla.com>
2020-12-31 16:26:26 +0200

    ack err fixed

--- spl-net/src/main/java/bgu/spl/net/impl/BGRSServer/Commands/ACKMessage.java
+++ spl-net/src/main/java/bgu/spl/net/impl/BGRSServer/Commands/ACKMessage.java
@@ -3,11 +3,35 @@ package bgu.spl.net.impl.BGRSServer.Commands;
 
 
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
 
-
+//TODO remove static
 public class ACKMessage {
-    public Serializable sendACK(String commandOpCode, String printMessage){
-        String result = "12" + commandOpCode + printMessage + "\0";
-        return  result;//UTF-16 by default
+    public static void main(String [] args){
+        Serializable s = sendACK("7", "PLEASE");
+
+    }
+    static Serializable sendACK(String commandOpCode, String printMessage){
+        byte[] stringMessage = printMessage.getBytes(StandardCharsets.UTF_8);
+        byte [] result = new byte[4+stringMessage.length + 1];
+        byte[] twoByte = shortToBytes((short)13);
+        result[0]= twoByte[0];
+        result[1]= twoByte[1];
+        twoByte = shortToBytes(Short.parseShort(commandOpCode));
+        result[2]= twoByte[0];
+        result[3]= twoByte[1];
+
+        System.arraycopy(stringMessage, 0, result, 4, result.length - 1 - 4); //copy string into result
+
+        result[result.length-1] = '\0';
+        return  result;
+    }
+    static byte[] shortToBytes(short num)
+    {
+        byte[] bytesArr = new byte[2];
+        bytesArr[0] = (byte)((num >> 8) & 0xFF);
+        bytesArr[1] = (byte)(num & 0xFF);
+        return bytesArr;
     }
 }
+
--- spl-net/src/main/java/bgu/spl/net/impl/BGRSServer/Commands/ERRMessage.java
+++ spl-net/src/main/java/bgu/spl/net/impl/BGRSServer/Commands/ERRMessage.java
@@ -1,10 +1,25 @@
 package bgu.spl.net.impl.BGRSServer.Commands;
 
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
 
 public class ERRMessage {
     public Serializable sendERR(String commandOpCode){
-        String result = "13" + commandOpCode;
-        return  result; //UTF-16 by default
+        byte [] result = new byte[4];
+        byte[] twoByte = shortToBytes((short)13);
+        result[0]= twoByte[0];
+        result[1]= twoByte[1];
+        twoByte = shortToBytes(Short.parseShort(commandOpCode));
+        result[2]= twoByte[0];
+        result[3]= twoByte[1];
+        return  result;
+    }
+
+    public byte[] shortToBytes(short num)
+    {
+        byte[] bytesArr = new byte[2];
+        bytesArr[0] = (byte)((num >> 8) & 0xFF);
+        bytesArr[1] = (byte)(num & 0xFF);
+        return bytesArr;
     }
 }
