e1094860b6c5e6da5f971de4ccac0918f35b557e
Lior Fradin <khtur9512300@walla.com>
2021-01-02 14:07:29 +0200

    added comments

--- Client/include/connectionHandler.h
+++ Client/include/connectionHandler.h
@@ -1,7 +1,6 @@
 #ifndef CONNECTION_HANDLER__
 #define CONNECTION_HANDLER__
-                                           
-#include <string>
+
 #include <iostream>
 #include <boost/asio.hpp>
 
@@ -28,26 +27,7 @@ public:
 	// Send a fixed number of bytes from the client - blocking.
     // Returns false in case the connection is closed before all the data is sent.
     bool sendBytes(const char bytes[], int bytesToWrite);
-/*
-    // Read an ascii line from the server
-    // Returns false in case connection closed before a newline can be read.
-    bool getLine(std::string& line);
-	
-	// Send an ascii line from the server
-    // Returns false in case connection closed before all the data is sent.
-    bool sendLine(std::string& line);
- 
-    // Get Ascii data from the server until the delimiter character
-    // Returns false in case connection closed before null can be read.
-    bool getFrameAscii(std::string& frame, char delimiter);
- 
-    // Send a message to the remote host.
-    // Returns false in case connection is closed before all the data is sent.
-    bool sendFrameAscii(const std::string& frame, char delimiter);
 
-    //TODO my code below make sure to remove excess later
-    bool sendFrameAscii(const std::string& frame);
-	*/
     // Close down the connection properly.
     void close();
  
