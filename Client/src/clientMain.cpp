4879d5c54543db96d273345723e10fa0295d5386
max <maximgel@post.bgu.ac.il>
2021-01-07 19:27:08 +0200

    clientMain - Thread terminating gracefully.

--- Client/src/clientMain.cpp
+++ Client/src/clientMain.cpp
@@ -10,14 +10,16 @@ using namespace std;
 class KeyboardListener {
 private:
     int _id;
-    std::mutex&_mutex;
-    bool& _session;
-    ConnectionHandler* connectionHandler;
+    std::mutex &_mutex;
+    bool &_session;
+    ConnectionHandler *connectionHandler;
 public:
-    KeyboardListener(int id, std::mutex &mutex, bool& session, ConnectionHandler* ch) : _id(id), _mutex(mutex), _session(session), connectionHandler(ch) {}
+    KeyboardListener(int id, std::mutex &mutex, bool &session, ConnectionHandler *ch) : _id(id), _mutex(mutex),
+                                                                                        _session(session),
+                                                                                        connectionHandler(ch) {}
 
     void run() {
-        while(_session) {
+        while (_session) {
             std::lock_guard<std::mutex> lock(_mutex);
             const short bufsize = 1024;
             char buf[bufsize];
@@ -33,71 +35,76 @@ public:
                 std::cout << "Disconnected. Exiting...\n" << std::endl;
                 break;
             }
+            if (line.compare("LOGOUT") == 0)
+                std::this_thread::sleep_for(std::chrono::milliseconds(20)); // Wait here till "_session" will be equal to false.
         }
     }
 };
 
-int main(int argc, char *argv[]) {
 
-    if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " host port" << std::endl << std::endl;
-        return -1;
-    }
-    std::string host = argv[1];
-    short port = atoi(argv[2]);
+    int main(int argc, char *argv[]) {
 
-    ConnectionHandler connectionHandler(host, port);
-    if (!connectionHandler.connect()) {
-        std::cerr << "Cannot connect to " << host << ":" << port << std::endl;
-        return 1;
-    }
-    else
-        std::cout << "Connection established to: " << host << ":" << port << std::endl;
-
-    bool session = true;
-    std::mutex mutex;
-    KeyboardListener kb(1, mutex, session, &connectionHandler);
-
-    //activate a thread that listens to keyboard
-    std::thread th1(&KeyboardListener::run, &kb);
-
-    //Session with server
-    while (session) {
-        char opCodeArr[2];
-        char messageOpCodeArr[2];
-        //Waiting for server reply, BLOCKING
-        if (!connectionHandler.getBytes(opCodeArr, 2)) {
-            std::cout << "Disconnected. Exiting...\n" << std::endl;
-            break;
+        if (argc < 3) {
+            std::cerr << "Usage: " << argv[0] << " host port" << std::endl << std::endl;
+            return -1;
         }
-        if (!connectionHandler.getBytes(messageOpCodeArr, 2)) {
-            std::cout << "Disconnected. Exiting...\n" << std::endl;
-            break;
-        }
-        short opCode = encdec::decodeTwoBytes(opCodeArr);
-        short opCodeMessage = encdec::decodeTwoBytes(messageOpCodeArr);
-        //Received acknowledge message
-        if(opCode == 12) {
-            cout << "ACK " << opCodeMessage << endl;
-            std::string str = "";
-            if(opCodeMessage == 4) //Logout
-                session = false;
-            if (!encdec::decodeString(connectionHandler, str)) {
+        std::string host = argv[1];
+        short port = atoi(argv[2]);
+
+        ConnectionHandler connectionHandler(host, port);
+        if (!connectionHandler.connect()) {
+            std::cerr << "Cannot connect to " << host << ":" << port << std::endl;
+            return 1;
+        } else
+            std::cout << "Connection established to: " << host << ":" << port << std::endl;
+
+        bool session = true;
+        std::mutex mutex;
+        KeyboardListener kb(1, mutex, session, &connectionHandler);
+
+        //activate a thread that listens to keyboard
+        std::thread th1(&KeyboardListener::run, &kb);
+
+        //Session with server
+        while (session) {
+            char opCodeArr[2];
+            char messageOpCodeArr[2];
+            //Waiting for server reply, BLOCKING
+            if (!connectionHandler.getBytes(opCodeArr, 2)) {
                 std::cout << "Disconnected. Exiting...\n" << std::endl;
                 break;
             }
-            //Cases which return a subsequent message to ACK
-            else if((opCodeMessage == 6) | (opCodeMessage == 7) | (opCodeMessage == 8) | (opCodeMessage == 9) | (opCodeMessage == 11)) {
-                cout << str << endl;
+            if (!connectionHandler.getBytes(messageOpCodeArr, 2)) {
+                std::cout << "Disconnected. Exiting...\n" << std::endl;
+                break;
             }
+            short opCode = encdec::decodeTwoBytes(opCodeArr);
+            short opCodeMessage = encdec::decodeTwoBytes(messageOpCodeArr);
+            //Received acknowledge message
+            if (opCode == 12) {
+                cout << "ACK " << opCodeMessage << endl;
+                std::string str = "";
+                if (opCodeMessage == 4) //Logout
+                    session = false;
+                if (!encdec::decodeString(connectionHandler, str)) {
+                    std::cout << "Disconnected. Exiting...\n" << std::endl;
+                    break;
+                }
+                    //Cases which return a subsequent message to ACK
+                else if ((opCodeMessage == 6) | (opCodeMessage == 7) | (opCodeMessage == 8) | (opCodeMessage == 9) |
+                         (opCodeMessage == 11)) {
+                    cout << str << endl;
+                }
+            }
+                //Received error message
+            else if (opCode == 13) {
+                cout << "ERROR " << opCodeMessage << endl;
+            } else cout << "Incorrect OpCode returned" << endl;
         }
-        //Received error message
-        else if(opCode == 13){
-            cout << "ERROR " << opCodeMessage << endl;
-        } else cout << "Incorrect OpCode returned" << endl;
+        //Session finished
+
+        th1.join();
+        return 0;
     }
-    //Session finished
-    th1.detach();
-    return 0;
-}
+
 
